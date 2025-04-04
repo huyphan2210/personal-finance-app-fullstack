b64b6594c1570dce5c6d26d0aab56107d52343b7
HuyPhanNC <huy.phan2210@outlook.com>
2025-04-04 15:26:25 +0700

    Use Nuxt

---
+++ client/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <NuxtRouteAnnouncer />
+  <NuxtPage />
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+:root {
+  --beige-100: #f8f4f0;
+  --beige-500: #98908b;
+
+  --grey-100: #f2f2f2;
+  --grey-300: #b3b3b3;
+  --grey-500: #696868;
+  --grey-900: #201f24;
+}
+
+body {
+  background-color: var(--beige-100);
+  min-height: 100vh;
+}
+</style>
