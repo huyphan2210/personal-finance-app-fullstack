dc3dd990ceb4532dc86b08c16464cf236cc8c2b1
HuyPhanNC <huy.phan2210@outlook.com>
2025-04-08 09:10:05 +0700

    Create default.scss and use it

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  css: ["./assets/styles/default.scss"],
   devtools: { enabled: true },
   pages: true,
+  modules: ["@pinia/nuxt"],
+  ssr: false,
 });
