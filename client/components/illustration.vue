dbf84e29a847e9ea0d0cb9ec229e83c57e0e5cf1
Huy Phan <huy.phan2210@outlook.com>
2025-04-13 08:20:28 +0700

    Add illutration image

--- client/components/illustration.vue
+++ client/components/illustration.vue
@@ -1,5 +1,11 @@
 <template>
-  <aside class="illustration"></aside>
+  <aside class="illustration">
+    <img
+      src="../assets/images/illustration-authentication.svg"
+      loading="lazy"
+      alt="Login Illustration"
+    />
+  </aside>
 </template>
 
 <style lang="scss" scoped>
@@ -7,11 +13,20 @@
   display: none;
   padding: 1.25rem;
   background-color: var(--white);
+  width: 40%;
+  max-width: 35rem;
+  max-height: calc(100vh - 2.5rem);
+
+  img {
+    width: 100%;
+    border-radius: 0.75rem;
+    background-color: var(--grey-900);
+  }
 }
 
-@media screen and (min-width: 96rem) {
+@media screen and (min-width: 64rem) {
   .illustration {
-    display: unset;
+    display: flex;
   }
 }
 </style>
