2377afa77324888fc3da241a913ea0de3448efbb
Huy Phan <huy.phan2210@outlook.com>
2025-04-13 08:20:39 +0700

    Update login.vue

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    
-  </div>
+  <form class="form--login">
+    <SharedPageHeading />
+  </form>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
+<style lang="scss" scoped>
+.form--login {
+  background-color: var(--white);
+  padding: 1.5rem 1.25rem;
+  width: clamp(303px, 73vw, 31rem);
+  margin: auto;
+  border-radius: 0.75rem;
+}
 
-<style>
-
+@media screen and (min-width: 48rem) {
+  .form--login {
+    padding: 2rem;
+  }
+}
 </style>
