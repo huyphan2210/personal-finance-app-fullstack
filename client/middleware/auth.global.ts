a342788944d5289d038b45823bd2226321c1fb57
HuyPhanNC <huy.phan2210@outlook.com>
2025-04-24 16:58:59 +0700

    Update auth.global.ts

--- client/middleware/auth.global.ts
+++ client/middleware/auth.global.ts
@@ -1,17 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-  const excludeRoutes = [Page.Login, Page.Signup]
-  if (excludeRoutes.includes(to.path as Page)) {
-    return
-  }
-
   const {isLoaded, isSignedIn} = useAuth();
   const checkLogin = (isAuthLoaded: boolean) => {
     if (!isAuthLoaded) return;
 
     if (!isSignedIn.value) {
       navigateTo(Page.Login)
-    } else {
-
     }
   }
   
