6a5805674743f9914a65688a82b63639b99e6e64
HuyPhanNC <huy.phan2210@outlook.com>
2025-04-28 10:54:22 +0700

    Add the minimize version of sidebar

--- client/components/shared/logo.vue
+++ client/components/shared/logo.vue
@@ -1,5 +1,26 @@
 <template>
-  <NuxtLink :to="Page.Overview"
-    ><img src="../../assets/images/logo.svg" alt="Logo"
-  /></NuxtLink>
+  <NuxtLink :to="Page.Overview">
+    <img class="logo_img--minimize" src="../../assets/images/logo--minimize.svg" alt="Logo" />
+    <img class="logo_img" src="../../assets/images/logo.svg" alt="Logo" />
+  </NuxtLink>
 </template>
+
+<style lang="scss" scoped>
+a {
+  container-type: inline-size;
+
+  .logo_img {
+    display: none;
+  }
+
+  @container (min-width: 88px) {
+    .logo_img {
+      display: unset;
+
+      &--minimize {
+        display: none;
+      }
+    }
+  }
+}
+</style>
