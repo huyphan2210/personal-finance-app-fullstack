273425bd8ce03b3d352084fdfaca11c309ce2356
huyphan2210 <huy.phan2210@outlook.com>
2025-04-29 09:34:48 +0700

    Add overview-summary-card

---
+++ client/components/overview/summary-card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="summary-card">
+    <small>{{ cardHeading }}</small>
+    <span>{{ cardContent }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { cardHeading, cardContent } = defineProps<{
+  cardHeading: string,
+  cardContent: string
+}>();
+</script>
+
+<style lang="scss" scoped>
+.summary-card {
+  background-color: var(--white);
+  padding: 1.25rem;
+  border-radius: 0.75rem;
+
+  small {
+    color: var(--grey-500);
+    @include text-preset-4;
+  }
+
+  span {
+    color: var(--grey-900);
+    display: block;
+    margin-top: 0.75rem;
+    @include text-preset-1;
+  }
+
+  &.main {
+    background-color: var(--grey-900);
+
+    small {
+      color: var(--white);
+    }
+
+    span {
+      color: var(--white);
+    }
+  }
+}
+</style>
