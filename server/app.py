f6dd05e596b916e3bb98724e55456f7797cdc879
huyphan2210 <huy.phan2210@outlook.com>
2025-05-05 09:44:26 +0700

    Use flask_cors

--- server/app.py
+++ server/app.py
@@ -1,10 +1,16 @@
 import json
 from flask import Flask
+from flask_cors import CORS
+import os
 
 from controllers.transactions_controller import transaction_bp
 from controllers.overview_controller import overview_bp
 
 app = Flask(__name__)
+
+ui_url = os.getenv("UI_URL", "http://localhost:3000")
+
+CORS(app, resources={r"/*": {"origins": ui_url}})
 
 app.register_blueprint(overview_bp)
 app.register_blueprint(transaction_bp)
