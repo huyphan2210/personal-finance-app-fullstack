7c44a8f6b4f08149dd2ce07adc26d18b2a6b6209
Huy Phan <huy.phan2210@outlook.com>
2025-06-03 18:49:29 +0700

    Update transactions.vue

--- client/pages/transactions.vue
+++ client/pages/transactions.vue
@@ -1,7 +1,56 @@
 <template>
   <shared-page-heading />
+  <section class="transactions">
+    <section class="transactions_filter"></section>
+    <table class="transactions_table">
+      <thead>
+        <tr>
+          <th>Recipient / Sender</th>
+          <th>Category</th>
+          <th>Transaction Date</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Recipient / Sender</th>
+          <th>Category</th>
+          <th>Transaction Date</th>
+          <th>Amount</th>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script setup lang="ts"></script>
 
-<style></style>
+<style lang="scss" scoped>
+.transactions {
+  flex: 1;
+  background-color: var(--white);
+  border-radius: 0.75rem;
+  padding: 1.5rem 1.25rem;
+  &_table {
+    table-layout: fixed;
+    width: 100%;
+    th, td {
+      text-align: start;
+      color: var(--grey-500);
+      @include text-preset-5;
+
+      &:last-child {
+        text-align: end;
+      }
+
+      &:not(:last-child):not(:first-child) {
+        display: none;
+      }
+    }
+
+    thead {
+      display: none;
+    }
+  }
+}
+</style>
